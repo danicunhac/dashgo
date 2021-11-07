dd8c150e03bd4fbe954879728eeb33a8b8b2e9b9
Daniel Cunha <danielcc.jp@gmail.com>
2021-11-07 10:59:09 -0300

    feat: header

---
+++ src/layout/dashboard.tsx
@@ -0,0 +1,11 @@
+import { Text } from '@chakra-ui/layout';
+import { Header } from '../common/Header';
+
+export default function Dashboard() {
+  return (
+    <>
+      <Header />
+      <Text>Dashboard</Text>
+    </>
+  );
+}
---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,5 @@
+import Dashboard from '../layout/dashboard';
+
+export default function DashboardPage() {
+  return <Dashboard />;
+}
